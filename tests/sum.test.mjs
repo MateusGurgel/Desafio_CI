ab332e2f5a2817ae71c7e64336158f7ee65c3b2e
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-09-10 13:21:51 -0300

    feat: add bug

--- tests/sum.test.mjs
+++ tests/sum.test.mjs
@@ -3,5 +3,5 @@ import assert from 'node:assert';
 import { sum } from '../modules/sum.mjs';
 
 test('sum test', (t) => {
-    assert.strictEqual(sum(1, 2), 3);
+    assert.strictEqual(sum(1, 2), 5);
   });
